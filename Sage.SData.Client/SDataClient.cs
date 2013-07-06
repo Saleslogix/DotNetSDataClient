5cc1a392a7ff12754444bf93bfecfe54a452a831
Nathan Baulch <nathan.baulch@gmail.com>
2013-07-06 22:41:11 +1000

    SDataClient and SDataResults implementations

---
+++ Sage.SData.Client/SDataClient.cs
@@ -0,0 +1,120 @@
+﻿using System.Net;
+using Sage.SData.Client.Content;
+using Sage.SData.Client.Framework;
+
+namespace Sage.SData.Client
+{
+    public class SDataClient : ISDataClient
+    {
+        private readonly CookieContainer _cookies = new CookieContainer();
+
+        public SDataClient(string uri)
+        {
+            Uri = uri;
+        }
+
+        public string Uri { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UserAgent { get; set; }
+        public int? Timeout { get; set; }
+        public int? TimeoutRetryAttempts { get; set; }
+        public IWebProxy Proxy { get; set; }
+        public ICredentials Credentials { get; set; }
+        public INamingScheme NamingScheme { get; set; }
+        public MediaType? Format { get; set; }
+        public string Language { get; set; }
+
+        public ISDataResults Execute(ISDataParameters parms)
+        {
+            var request = CreateRequest(parms);
+            return SDataResults.FromResponse(request.GetResponse());
+        }
+
+        public ISDataResults<T> Execute<T>(ISDataParameters parms)
+        {
+            var request = CreateRequest(parms);
+            var response = request.GetResponse();
+            return CreateResults<T>(response);
+        }
+
+        private SDataRequest CreateRequest(ISDataParameters parms)
+        {
+            var uri = new SDataUri(Uri)
+                          {
+                              StartIndex = parms.StartIndex,
+                              Count = parms.Count,
+                              Where = parms.Where,
+                              OrderBy = parms.OrderBy,
+                              Search = parms.Search,
+                              Include = parms.Include,
+                              Precedence = parms.Precedence,
+                              Select = parms.Select,
+                              TrackingId = parms.TrackingId,
+                              Language = parms.Language ?? Language,
+                              Version = parms.Version
+                          };
+            if (parms.Path != null)
+            {
+                uri.AppendPath(parms.Path);
+            }
+            if (parms.Format != null)
+            {
+                uri.Format = parms.Format.Value;
+            }
+            else if (Format != null)
+            {
+                uri.Format = Format.Value;
+            }
+            if (parms.IncludeSchema != null)
+            {
+                uri.IncludeSchema = parms.IncludeSchema.Value;
+            }
+            if (parms.ReturnDelta != null)
+            {
+                uri.ReturnDelta = parms.ReturnDelta.Value;
+            }
+            var operation = new RequestOperation(parms.Method, parms.Content)
+                                {
+                                    Selector = parms.Selector,
+                                    ContentType = parms.ContentType,
+                                    ETag = parms.ETag
+                                };
+            foreach (var item in parms.Form)
+            {
+                operation.Form.Add(item.Key, item.Value);
+            }
+            foreach (var file in parms.Files)
+            {
+                operation.Files.Add(file);
+            }
+            var request = new SDataRequest(uri.ToString())
+                              {
+                                  UserName = UserName,
+                                  Password = Password,
+                                  UserAgent = UserAgent,
+                                  Proxy = Proxy,
+                                  Credentials = Credentials,
+                                  NamingScheme = NamingScheme,
+                                  Cookies = _cookies,
+                                  Accept = parms.Accept
+                              };
+
+            if (Timeout != null)
+            {
+                request.Timeout = Timeout.Value;
+            }
+            if (TimeoutRetryAttempts != null)
+            {
+                request.TimeoutRetryAttempts = TimeoutRetryAttempts.Value;
+            }
+            return request;
+        }
+
+        private ISDataResults<T> CreateResults<T>(SDataResponse response)
+        {
+            var content = ContentHelper.Deserialize<T>(response.Content, NamingScheme);
+            return SDataResults.FromResponse(response, content);
+        }
+    }
+}
---
+++ Sage.SData.Client/SDataResults.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using System.Net;
+using Sage.SData.Client.Framework;
+
+namespace Sage.SData.Client
+{
+    public class SDataResults : ISDataResults
+    {
+        private readonly HttpStatusCode _statusCode;
+        private readonly MediaType? _contentType;
+        private readonly string _eTag;
+        private readonly string _location;
+        private readonly IList<AttachedFile> _files;
+
+        public static ISDataResults FromResponse(SDataResponse response)
+        {
+            return new SDataResults(response.StatusCode,
+                                    response.ContentType,
+                                    response.ETag,
+                                    response.Location,
+                                    response.Files);
+        }
+
+        public static ISDataResults<T> FromResponse<T>(SDataResponse response, T content)
+        {
+            return new SDataResults<T>(response.StatusCode,
+                                       response.ContentType,
+                                       response.ETag,
+                                       response.Location,
+                                       response.Files,
+                                       content);
+        }
+
+        internal SDataResults(HttpStatusCode statusCode,
+                              MediaType? contentType,
+                              string eTag,
+                              string location,
+                              IList<AttachedFile> files)
+        {
+            _statusCode = statusCode;
+            _contentType = contentType;
+            _eTag = eTag;
+            _location = location;
+            _files = files;
+        }
+
+        public HttpStatusCode StatusCode
+        {
+            get { return _statusCode; }
+        }
+
+        public MediaType? ContentType
+        {
+            get { return _contentType; }
+        }
+
+        public string ETag
+        {
+            get { return _eTag; }
+        }
+
+        public string Location
+        {
+            get { return _location; }
+        }
+
+        public IList<AttachedFile> Files
+        {
+            get { return _files; }
+        }
+    }
+
+    public class SDataResults<T> : SDataResults, ISDataResults<T>
+    {
+        private readonly T _content;
+
+        internal SDataResults(HttpStatusCode statusCode,
+                              MediaType? contentType,
+                              string eTag,
+                              string location,
+                              IList<AttachedFile> files,
+                              T content)
+            : base(statusCode, contentType, eTag, location, files)
+        {
+            _content = content;
+        }
+
+        public T Content
+        {
+            get { return _content; }
+        }
+    }
+}
