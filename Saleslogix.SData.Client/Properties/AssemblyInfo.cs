6602c0489b4d1e5ff2d4a7eb67bf3417a1d3f38f
Nathan Baulch <nathan.baulch@gmail.com>
2014-06-06 03:01:03 +1000

    Strongly name the client and test assemblies

--- Saleslogix.SData.Client/Properties/AssemblyInfo.cs
+++ Saleslogix.SData.Client/Properties/AssemblyInfo.cs
@@ -25,8 +25,13 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.0.1530")]
-[assembly: AssemblyFileVersion("2.0.0.1530")]
+[assembly: AssemblyVersion("2.0.0.1531")]
+[assembly: AssemblyFileVersion("2.0.0.1531")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyInformationalVersion("2.0")]
-[assembly: InternalsVisibleTo("Saleslogix.SData.Client.Test")]
+[assembly: InternalsVisibleTo("Saleslogix.SData.Client.Test, PublicKey=" +
+                              "00240000048000009400000006020000002400005253413100040000010001004f6d7cc2aca5bf" +
+                              "a8d454d328b17b843b6e660947a1b16f179307ff46cba02baef4670c51eba6a98e724b95e43719" +
+                              "ba64860cfbc229fc25234cfcfdf25f65df85eb8b0c6bad0aa6fa5b9bf945ef5062aa131ba5939d" +
+                              "170c2a059860a64281f71cd8662f816f1fe0e7263c701408a55c30923ee0620ab622ec7f6fbf73" +
+                              "dfb01894")]
